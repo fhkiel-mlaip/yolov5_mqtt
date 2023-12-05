99d891ee2262a3b34a1fb43358b480973b351c89
David <kieljodel@gmail.com>
2023-12-05 11:00:19 +0000

    Initial commit

--- start_multiple.py
+++ start_multiple.py
@@ -1,12 +1,12 @@
 import subprocess
 
 # Define the parameters for your_script.py
-camera_names = ["Bug", "Heck", "Backboard", "Steuerboard"]
+camera_names = ["camera1", "camera2", "camera3", "camera4"]
 rtsp_urls = [
-    "rtsp://192.168.237.1:5541/0554eebe-d674-46b2-b7c4-df88dac3aa2e/0",
-    "rtsp://192.168.237.1:5541/54bd800f-4ebb-43ad-a3e0-cf5258c8d7cc/0",
-    "rtsp://192.168.237.1:5541/45e9c602-a351-4395-82d1-e514323da00f/0",
-    "rtsp://192.168.237.1:5541/ea180e4c-266f-425e-9163-5e3ffc94fd88/0"
+    "rtsp://camera1_ip_address:554/1/h264major",
+    "rtsp://camera2_ip_address:554/1/h264major",
+    "rtsp://camera3_ip_address:554/1/h264major",
+    "rtsp://camera4_ip_address:554/1/h264major",
 ]
 
 # Loop through the parameters and start your_script.py for each combination
